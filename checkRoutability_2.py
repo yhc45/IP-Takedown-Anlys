4b2e7f8fb438b11c94496bd07472b3c1ccb4613a
yhc45 <22433581+yhc45@users.noreply.github.com>
2020-04-09 19:20:46 -0700

    add code to check routability

---
+++ checkRoutability_2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import cPickle as pickle
+import ipaddress
+from treeOp import buildTree
+from datetime import datetime, date, timedelta
+import random
+
+def daterange (start_date, end_date):
+  for n in range(int((end_date-start_date).days)):
+    yield start_date + timedelta(n)
+
+
+
+def main():
+  start_date = date(2019,6,5)
+  end_date = date(2020,2,26)
+  drop_route = {}
+  drop_counter = 0
+  weird_counter = 0
+  diff_time = 0
+  diff_amount = 0
+  diff_weird_counter = 0
+  occurred = set()
+  with open("droplist_data.pickle", "rb") as f:
+    drop_data = pickle.load(f)
+  ip_list = [x for x in drop_data if x[-3] == '/']
+  ip_list = ip_list[len(ip_list)//2:]
+  for p_date in daterange(start_date,end_date):
+    day = p_date.strftime('%d')
+    month = p_date.strftime('%m')
+    year = p_date.strftime('%Y')
+    date_str = p_date.strftime('%Y-%m-%d')
+    try: 
+      tree = buildTree(year,month,day)
+    except OSError as e:
+      print(e)
+      print('failed to access date %s'%date_str)
+      print('Make sure the date is valid')
+      continue
+
+    print(date_str)
+    for x in ip_list:
+      counter = 0
+      if x not in occurred:
+        for u_ip in random.sample(list(ipaddress.IPv4Network(unicode(x, "utf-8"))),100):
+          ip = str(u_ip)
+          if not tree.check_prefix_is_routable(ip):
+            counter += 1
+        if counter != 0:
+          if date_str not in drop_route:
+            drop_route[date_str] = [x]
+            occurred.add(x)
+            drop_counter += 1
+          else:
+            drop_route[date_str].append(x)
+            occurred.add(x)
+            drop_counter += 1
+
+          date_added = datetime.strptime(drop_data[x][0],"%Y-%m-%d").date()
+          diff = p_date - date_added
+          if p_date < date_added:
+            # blocked before it is added to bllack list
+            diff_weird_counter += 1
+          else:
+            diff_time += diff.total_seconds()
+            diff_amount +=1
+          if counter != 100:
+            weird_counter += 1
+  f1 = open( "droproute_2.pickle", "wb" )
+  pickle.dump( drop_route, f1)
+  f1.close()
+
+  avg_day = diff_time/86400/diff_amount
+  print("drop_part2")
+  print("drop_counter: " + str(drop_counter))
+  print(drop_route)
+  print("weird_counter " + str(weird_counter))
+  print("avg_hour " + str(avg_day))
+  print("diff_weird_counter " + str(diff_weird_counter))
+
+if __name__ == "__main__":
+  main()
